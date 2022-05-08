bc7a66f38138bbfc102cb63d38d774534507a67e
“JLopez-W” <102275514+JLopez-W@users.noreply.github.com>
2022-05-08 15:06:09 +0700

    Add playerPlay function and playSingle function.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,38 @@
 
 let items = ['rock','paper','scissors'];
 
-function computerPlay(items) {
+function playerPlay() {
     return items[Math.floor(Math.random()*items.length)];
 }
 
-console.log(computerPlay(items));
+function computerPlay() {
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+let playerSelection = playerPlay();
+let computerSelection = computerPlay();
+
+
+function playSingle(_playerSelection, _computerSelection) {
+        
+    if (playerSelection === 'rock'  &&  computerSelection === 'scissors') {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beat Paper!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "You lose! Scissors beat Paper!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats Scissors!";
+    } else {
+        return "It's a tie!";
+    }
+
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playSingle());
