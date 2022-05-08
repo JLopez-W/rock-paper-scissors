dbb47f73ae8e9c93b7ea02390114bebc370b7c5a
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-05-08 23:25:34 +0700

    Change default return message for playSingle.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 let items = ['ROCK','PAPER','SCISSORS'];
 
-let playerSelection = prompt("Choose your item! Rock, Paper, or Scissors?").toUpperCase();
+let playerSelection = prompt('Choose your item! Rock, Paper, or Scissors?').toUpperCase();
 
 function computerPlay() {
     return items[Math.floor(Math.random()*items.length)];
@@ -9,22 +9,25 @@ function computerPlay() {
 
 let computerSelection = computerPlay();
 
+
 function playSingle(_playerSelection, _computerSelection) {
         
     if (playerSelection === 'ROCK'  &&  computerSelection === 'SCISSORS') {
-        return "You win! ROCK beats SCISSORS!";
+        return 'You win! ROCK beats SCISSORS!';
     } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return "You win! PAPER beats ROCK!";
+        return 'You win! PAPER beats ROCK!';
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return "You win! SCISSORS beat PAPER!";
+        return 'You win! SCISSORS beat PAPER!';
     } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return "You lose! PAPER beats ROCK!";
+        return 'You lose! PAPER beats ROCK!';
     } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return "You lose! SCISSORS beat PAPER!";
+        return 'You lose! SCISSORS beat PAPER!';
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return "You lose! ROCK beats SCISSORS!";
+        return 'You lose! ROCK beats SCISSORS!'; 
+    } else if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
     } else {
-        return "It's a tie!";
+        return 'What? Please try again.';
     }
 
 }
@@ -32,3 +35,4 @@ function playSingle(_playerSelection, _computerSelection) {
 console.log(playerSelection);
 console.log(computerSelection);
 console.log(playSingle());
+
