ef2f58300c8ff94b3534adb10f65cf440bbc1400
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-05-11 23:09:36 +0700

    Reduce number of else-if statements in playSingle function.

--- javascript.js
+++ javascript.js
@@ -12,22 +12,14 @@ let computerSelection = computerPlay();
 
 function playSingle(playerSelection, computerSelection) {
         
-    if (playerSelection === 'ROCK'  &&  computerSelection === 'SCISSORS') {
-        return 'You win! ROCK beats SCISSORS!';
-    } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return 'You win! PAPER beats ROCK!';
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return 'You win! SCISSORS beat PAPER!';
-    } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return 'You lose! PAPER beats ROCK!';
-    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return 'You lose! SCISSORS beat PAPER!';
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return 'You lose! ROCK beats SCISSORS!'; 
-    } else if (playerSelection === computerSelection) {
-        return 'It\'s a tie!';
-    } else {
-        return 'What? Please try again.';
+    if ((playerSelection === 'ROCK'  &&  computerSelection === 'SCISSORS') || (playerSelection === 'PAPER' && computerSelection === 'ROCK') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
+        return 'Player Point!' + ' ' + playerSelection + ' ' + 'beats' + ' ' + computerSelection + '' + '!';
+     } else if ((playerSelection === 'PAPER' && computerSelection === 'SCISSORS') || (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') || (playerSelection === 'ROCK' && computerSelection === 'PAPER')) {
+          return 'Computer Point!' + ' ' + computerSelection + ' ' + 'beats' + ' ' + playerSelection + '' + '!'; 
+     } else if (playerSelection === computerSelection) {
+         return 'It\'s a tie! No score.';
+     } else {
+         return 'What? Please try again.';
     }
 
 }
@@ -35,4 +27,3 @@ function playSingle(playerSelection, computerSelection) {
 console.log(playerSelection);
 console.log(computerSelection);
 console.log(playSingle(playerSelection, computerSelection));
-
