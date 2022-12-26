d31ff55583bbc61bcdaa82010fe90a8ad2ab500a
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2022-12-26 21:22:01 +0700

    Rework 5-Round Game
    Move Button Event Listener to Bottom of Script
    
    Move Score Checking and Conditionals to Event Listener
    
    Remove game() Function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,6 @@
 
 const items = ['ROCK','PAPER','SCISSORS'];
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      playerSelection = button.id;
-      computerSelection = computerPlay();
-      playRound(playerSelection,computerSelection);
-    });
-});
 
 const container = document.querySelector('#container');
 const results = document.createElement('div');
@@ -19,7 +12,6 @@ function playerScoreAdd1() {
   playerScore += 1;
 }
 
-
 let computerScore = 0;
 function computerScoreAdd1() {
     computerScore += 1;
@@ -29,15 +21,11 @@ function checkScore() {
     return playerScore + computerScore;
  }
 
-
-let computerSelection = computerPlay();
-
-
-
 function computerPlay() {
     return items[Math.floor(Math.random()*items.length)];
 }
 
+let computerSelection = computerPlay();
 let score = checkScore();
 //game(score);
 
@@ -51,26 +39,26 @@ function playRound(playerSelection, computerSelection) {
       results.textContent = 'Computer Point!' + ' ' + computerSelection + ' ' + 'beats' + ' ' + playerSelection + '' + '!'+ ' ' + playerScore + ' ' + 'vs' + ' ' + computerScore; 
   } else if (playerSelection === computerSelection) {
       results.textContent = 'It\'s a tie! No score.';  
-  } else {
-      results.textContent = 'What? Please try again.';
   }
-} 
+}
+//   } else {
+//       results.textContent = 'What? Please try again.';
+//   }
 
 
-// function game() { 
-//     for (let i = score; i <= 5; i++) {  
-//         if (playerScore < 3  && computerScore < 3) {  
-//              playerSelection = playerPlay();
-//              computerSelection = computerPlay();
-//              playRound(playerSelection, computerSelection);
-//              score = checkScore();
-//         } else if (playerScore === 3) {
-//             return ('You Win!' + ' ' + playerScore + ' ' + 'vs' + ' ' + computerScore);
-//         } else if (computerScore === 3) {
-//             return ('You Lose!' + ' ' + playerScore + ' ' + 'vs' + ' ' + computerScore);
-           
-//         }
-//     }
-// }
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = computerPlay();
+        score;
+        if (playerScore < 3  && computerScore < 3) {        
+         playRound(playerSelection, computerSelection); 
+        } else if (playerScore === 3) {
+            results.textContent = 'You Win!' + ' ' + playerScore + ' ' + 'vs' + ' ' + computerScore;
+        } else if (computerScore === 3) {
+            results.textContent = 'You Lose!' + ' ' + playerScore + ' ' + 'vs' + ' ' + computerScore;
+        }
+    });
+    });
 
-// console.log(game()):
+
